4eb24cdc1e41fdbcc4d032b1d465e3333d9af8f6
xogus7 <xogus2676@gmail.com>
2024-09-02 11:06:01 +0900

    Fix: 크로스헤어가 인식하는 픽셀값 조정
    - 크로스헤어가 인식하는 픽셀 값 조정 완료
    - 크로스헤어가 이동 가능한 좌표 범위 제한

--- src/pages/ImageScreen.jsx
+++ src/pages/ImageScreen.jsx
@@ -72,206 +72,243 @@ const ImageScreen = () => {
 		setColor(event.nativeEvent.data);
 	}, []);
 
+	const handleColorRecommend = () =>
+		navigation.navigate('ColorRecommendScreen', { color });
+
 	const getHtmlContent = imgSrc => `
 <html>
-  <head>
+<head>
     <style>
-      body {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-        background-color: ${COLOR.SECONDARY}
-      }
-      #canvas {
-        width: 100%;
-        height: auto;
-        max-height: 100%;
-      }
-      #crosshair {
-        position: absolute;
-        width: 100px;
-        height: 100px;
-        border-radius: 50%;
-        cursor: pointer;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-      .outerblackborder {
-        width: 90%;
-        height: 90%;
-        border-radius: 50%;
-        border: 3px solid;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: ${COLOR.SECONDARY};
+            position:relative;
+        }
 
-      #betweenbg {
-        width: 82%;
-        height: 82%;
-        border-radius: 50%;
-        border: 9px solid black;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
+        #canvas {
+            width: auto;
+            max-width: 100%;
+            height: auto;
+            max-height: 100%;
+        }
 
-      .outerwhiteborder {
-        margin: auto;
-        padding: auto;
-        width: 92%;
-        height: 92%;
-        border-radius: 50%;
-        border: 2px solid white;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
+        #crosshair {
+            position: absolute;
+            width: 100px;
+            height: 100px;
+            border-radius: 50%;
+            cursor: pointer;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
 
-      #innerblackborder {
-        width: 15%;
-        height: 15%;
-        border-radius: 50%;
-        border: 2px solid black;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background-color: white;
-      }
+        .outerblackborder {
+            width: 90%;
+            height: 90%;
+            border-radius: 50%;
+            border: 3px solid;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        #betweenbg {
+            width: 82%;
+            height: 82%;
+            border-radius: 50%;
+            border: 9px solid black;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .outerwhiteborder {
+            margin: auto;
+            padding: auto;
+            width: 92%;
+            height: 92%;
+            border-radius: 50%;
+            border: 2px solid white;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        #innerblackborder {
+            width: 15%;
+            height: 15%;
+            border-radius: 50%;
+            border: 2px solid black;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: white;
+        }
     </style>
-  </head>
-  <body>
+</head>
+
+<body>
     <canvas id="canvas"></canvas>
     <div id="crosshair">
-      <div class="outerblackborder">
-        <div id="betweenbg">
-          <div class="outerwhiteborder">
-            <div id="innerblackborder">
-              <div class="innerwhiteborder"></div>
-          </div>
+        <div class="outerblackborder">
+            <div id="betweenbg">
+                <div class="outerwhiteborder">
+                    <div id="innerblackborder">
+                        <div class="innerwhiteborder"></div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-  </div>
     </div>
     <script>
-      const img = new Image();
-      img.crossOrigin = 'Anonymous';
-      img.src = "${imgSrc}";
-      let previousDistance = 0;
-      
-      img.onload = function() {
-        const canvas = document.getElementById('canvas');
-        const ctx = canvas.getContext('2d');
-        const maxWidth = window.innerWidth;
-        const maxHeight = window.innerHeight;            
-        const ratio = Math.min(maxWidth / img.width, maxHeight / img.height);
-        canvas.width = img.width * ratio;
-        canvas.height = img.height * ratio;
-        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        const img = new Image();
+        img.crossOrigin = 'Anonymous';
+        img.src = "${imgSrc}";
+        let previousDistance = 0;
 
-        let isDragging = true;
-        let startX, startY;
+        img.onload = function () {
+            const canvas = document.getElementById('canvas');
+            const ctx = canvas.getContext('2d');
+            const maxWidth = window.innerWidth;
+            const maxHeight = window.innerHeight;
+            const ratio = Math.min(maxWidth / img.width, maxHeight / img.height);
 
-        function startDragging(e) {
-          isDragging = true;
-          startX = e.touches[0].clientX - crosshair.offsetLeft;
-          startY = e.touches[0].clientY - crosshair.offsetTop;
-        } 
-    
-        function drag(e) {
-          if (isDragging) {
-            e.preventDefault();
-            const clientX = e.touches[0].clientX;
-            const clientY = e.touches[0].clientY;
-            const x = clientX - startX;
-            const y = clientY - startY;
-            crosshair.style.left = x + 'px';
-            crosshair.style.top = y + 'px';
-            const maxX = canvas.width;
-            const maxY = canvas.height;
-            crosshair.style.left = Math.max(0, Math.min(x, maxX)) + 'px';
-            crosshair.style.top = Math.max(0, Math.min(y, maxY)) + 'px';
-            updateColor();
-          }
+            canvas.width = img.width * ratio;
+            canvas.height = img.height * ratio;
+            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+            let isDragging = true;
+            let startX, startY;
+
+
+
+			function limitLeft(x) {
+    			const halfWidth = crosshair.offsetWidth / 2;
+    			const canvasRightLimit = canvas.width - halfWidth;
+    			const canvasLeftLimit = -halfWidth;
+
+    			if (maxWidth > canvas.width) {
+        			const offset = (maxWidth - canvas.width) / 2;
+        			const minX = canvasLeftLimit + offset;
+        			const maxX = canvasRightLimit + offset;
+        			return Math.max(minX, Math.min(x, maxX - 1));
+    			} else {
+        			return Math.max(canvasLeftLimit, Math.min(x, canvasRightLimit - 1));
+    			}
+			}
+			
+			function limitTop(y) {
+    			const halfHeight = crosshair.offsetHeight / 2;
+    			const canvasBottomLimit = canvas.height - halfHeight;
+    			const canvasTopLimit = -halfHeight;
+
+    			if (maxHeight > canvas.height) {
+       				const offset = (maxHeight - canvas.height) / 2;
+        			const minY = canvasTopLimit + offset;
+        			const maxY = canvasBottomLimit + offset;
+        			return Math.max(minY, Math.min(y, maxY - 1));
+    			} else {
+        			return Math.max(canvasTopLimit, Math.min(y, canvasBottomLimit - 1));
+    			}
+			}
+			
+			
+            function startDragging(e) {
+                isDragging = true;
+                startX = e.touches[0].clientX - crosshair.offsetLeft;
+                startY = e.touches[0].clientY - crosshair.offsetTop;
+            }
+
+            function drag(e) {
+                if (isDragging) {
+                    e.preventDefault();
+                    const clientX = e.touches[0].clientX;
+                    const clientY = e.touches[0].clientY;
+                    const x = clientX - startX;
+                    const y = clientY - startY;
+                    crosshair.style.left = limitLeft(x) + 'px';
+                    crosshair.style.top = limitTop(y) + 'px';
+                    updateColor();
+                }
+            }
+
+            function stopDragging() {
+                isDragging = false;
+            }
+
+            function getDistance(touch1, touch2) {
+                const dx = touch2.clientX - touch1.clientX;
+                const dy = touch2.clientY - touch1.clientY;
+                return Math.sqrt(dx * dx + dy * dy);
+            }
+
+            function handleCanvasTouchStart(e) {
+
+                isDragging = true;
+                if (e.touches.length === 2) {
+                    isDragging = false;
+                    previousDistance = getDistance(e.touches[0], e.touches[1]);
+                }
+                const touchX = e.touches[0].clientX - crosshair.offsetWidth / 2;
+                const touchY = e.touches[0].clientY - crosshair.offsetHeight / 2 ;
+				crosshair.style.left = limitLeft(touchX) + 'px';
+                crosshair.style.top = limitTop(touchY) + 'px';
+                updateColor();
+            }
+
+            function handleCanvasTouchMove(e) {
+                if (e.touches.length === 2) {
+                    const currentDistance = getDistance(e.touches[0], e.touches[1]);
+                    const scaleFactor = currentDistance / previousDistance;
+                    scale *= scaleFactor;
+                    previousDistance = currentDistance;
+
+                    canvas.style.transform = "scale(1)";
+                } else if (isDragging) {
+                    e.preventDefault();
+                    const clientX = e.touches[0].clientX;
+                    const clientY = e.touches[0].clientY;
+                    const x = clientX - startX;
+                    const y = clientY - startY;
+                    crosshair.style.left = limitLeft(x) + 'px';
+                    crosshair.style.top = limitTop(y) + 'px';
+                    updateColor();
+                }
+            }
+
+            function handleCanvasTouchEnd(e) {
+                isDragging = false;
+                previousDistance = 0;
+            }
+
+            // 터치 이벤트
+            canvas.addEventListener('touchstart', handleCanvasTouchStart);
+            canvas.addEventListener('touchmove', handleCanvasTouchMove);
+            canvas.addEventListener('touchend', handleCanvasTouchEnd);
+
+            crosshair.addEventListener('touchstart', startDragging);
+            crosshair.addEventListener('touchmove', drag);
+            crosshair.addEventListener('touchend', stopDragging);
+
+            function updateColor() {
+                const rect = crosshair.getBoundingClientRect();
+                const x = (rect.left + rect.width / 2 + (canvas.width - maxWidth) / 2);
+                const y = (rect.top + rect.height / 2 + (canvas.height - maxHeight) / 2);
+                const pixel = ctx.getImageData(x, y, 1, 1).data;
+                const color = '#' + ((1 << 24) + (pixel[0] << 16) + (pixel[1] << 8) + pixel[2]).toString(16).slice(1);
+                betweenbg.style.borderColor = color;
+                window.ReactNativeWebView.postMessage(color);
+            }
         }
-
-        function stopDragging() {
-          isDragging = false;
-        }
-
-         
-
-      function getDistance(touch1, touch2) {
-        const dx = touch2.clientX - touch1.clientX;
-        const dy = touch2.clientY - touch1.clientY;
-        return Math.sqrt(dx * dx + dy * dy);
-      }
-
-      function handleCanvasTouchStart(e) {
-      
-        isDragging = true;
-        if (e.touches.length === 2) {
-          isDragging = false;
-          previousDistance = getDistance(e.touches[0], e.touches[1]);
-        }
-        const touchX = e.touches[0].clientX;
-        const touchY = e.touches[0].clientY;
-
-        crosshair.style.left = touchX - crosshair.offsetWidth / 2 + 'px';
-        crosshair.style.top = touchY - crosshair.offsetHeight / 2 + 'px';
-        updateColor(); 
-      }
-
-      function handleCanvasTouchMove(e) {
-        if (e.touches.length === 2) {
-          const currentDistance = getDistance(e.touches[0], e.touches[1]);
-          const scaleFactor = currentDistance / previousDistance;
-          scale *= scaleFactor;
-          previousDistance = currentDistance;
-
-          canvas.style.transform = "scale(1)";
-        } else if (isDragging) {
-          e.preventDefault();
-          const clientX = e.touches[0].clientX;
-          const clientY = e.touches[0].clientY;
-          const x = clientX - startX;
-          const y = clientY - startY;
-          crosshair.style.left = x + 'px';
-          crosshair.style.top = y + 'px';
-          updateColor();
-        }
-      }
-
-      function handleCanvasTouchEnd(e) {
-        isDragging = false;
-        previousDistance = 0;
-      }
-
-        // 터치 이벤트
-        canvas.addEventListener('touchstart', handleCanvasTouchStart);
-        canvas.addEventListener('touchmove', handleCanvasTouchMove);
-        canvas.addEventListener('touchend', handleCanvasTouchEnd);
-        
-        crosshair.addEventListener('touchstart', startDragging);
-        crosshair.addEventListener('touchmove', drag);
-        crosshair.addEventListener('touchend', stopDragging);
-
-        function updateColor() {
-          const rect = crosshair.getBoundingClientRect();
-          const x = Math.floor(rect.left + rect.width / 2 - canvas.offsetLeft);
-          const y = Math.floor(rect.top + rect.height / 2 - canvas.offsetTop);
-          const pixel = ctx.getImageData(x, y, 1, 1).data;
-          const color = '#' + ((1 << 24) + (pixel[0] << 16) + (pixel[1] << 8) + pixel[2]).toString(16).slice(1);
-          betweenbg.style.borderColor = color;
-          window.ReactNativeWebView.postMessage(color);
-        }
-      }
     </script>
-  </body>
+</body>
+>>>>>>> Stashed changes
 </html>
 `;
 
@@ -292,7 +329,7 @@ const ImageScreen = () => {
 					<Text style={styles.colorName}>≈색상 이름</Text>
 					<Text style={styles.colorName}>color name</Text>
 					<Text style={styles.colorHex}>
-						hex: {color.replace('#', '').toLocaleUpperCase()}
+						hex: {color.toLocaleUpperCase()}
 					</Text>
 					<Text style={styles.colorHsl}>hsl: 18.5, 5.2%, 48.8%</Text>
 				</View>
@@ -316,7 +353,9 @@ const ImageScreen = () => {
 				<TouchableOpacity style={styles.aiButton}>
 					<Text style={styles.aiButtonText}>AI 추천</Text>
 				</TouchableOpacity>
-				<TouchableOpacity style={styles.recommendationButton}>
+				<TouchableOpacity
+					style={styles.recommendationButton}
+					onPress={handleColorRecommend}>
 					<Text style={styles.recommendationButtonText}>
 						색상 추천
 					</Text>
