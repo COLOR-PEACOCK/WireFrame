2b6f8c5647cb8b5d634daaf5b8df911ca2c27297
xogus7 <xogus2676@gmail.com>
2024-09-05 11:14:38 +0900

    Design: Add icon svg

---
+++ src/assets/icons/index.js
@@ -0,0 +1,10 @@
+import CameraSVG from '@icons/camera.svg'
+import ImageSVG from '@icons/image.svg'
+import AISVG from '@icons/ai.svg'
+import PaletteSVG from '@icons/palette.svg'
+import ReportSVG from '@icons/report.svg'
+import Img from '@icons/img.png'
+import Img2 from '@icons/img2.png'
+import Img3 from '@icons/img3.png'
+
+export { CameraSVG, ImageSVG, AISVG, PaletteSVG, ReportSVG, Img, Img2, Img3 }
