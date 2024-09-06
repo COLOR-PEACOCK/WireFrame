826cc8a56f519b5787426c7cbfbdfb62b03c9350
xogus7 <xogus2676@gmail.com>
2024-09-06 19:00:47 +0900

    Feat: 색상 이름 자동완성 검색 기능

--- src/components/Home/SearchModal.jsx
+++ src/components/Home/SearchModal.jsx
@@ -1,16 +1,19 @@
 import { COLOR } from '@styles/color';
 import {
-	FlatList,
 	Modal,
 	Pressable,
+	ScrollView,
 	StyleSheet,
 	TouchableOpacity,
 	View,
 } from 'react-native';
 import { CustomText as Text } from '@components/common/CustomText';
-import { SearchInputForm, ListValue, Dropdown } from '@components/Home';
+import { SearchInputForm, ListValue, Dropdown, PressButton } from '@components/Home';
 import { useEffect, useState } from 'react';
 import { cmykToHex, hslToHex, rgbToHex } from '@utils/convertToHex';
+import { ArrowGoBackSVG, Formkit_submitSVG } from '@icons';
+import { isValidHexCode, isValidKorean } from '@utils/inputValidation';
+import useColorName from '@hooks/useColorName';
 
 const SearchModal = ({
 	list,
@@ -28,24 +31,84 @@ const SearchModal = ({
 		part3: '',
 		part4: '',
 	});
+	const [searchNameList, setSearchNameList] = useState([]);
+	const { getSearchColorList } = useColorName();
+	const { contentColor, buttonColor, handleTouchStart, handleTouchEnd } =
+		usePressButtonState();
 
+	// 입력 정보 처리
 	const handlePressSearch = () => {
 		const converters = {
-			'HEX': (values) => `#${values.part1}`,
-			'RGB': (values) => rgbToHex(values.part1, values.part2, values.part3),
-			'HSL': (values) => hslToHex(values.part1, values.part2, values.part3),
-			'CMYK': (values) => cmykToHex(values.part1, values.part2, values.part3, values.part4),
-			'색상 이름': (values) => values.part1,
+			HEX: values =>
+				isValidHexCode(`#${values.part1}`) ? `#${values.part1}` : null,
+			RGB: values => rgbToHex(values.part1, values.part2, values.part3),
+			HSL: values => hslToHex(values.part1, values.part2, values.part3),
+			CMYK: values =>
+				cmykToHex(
+					values.part1,
+					values.part2,
+					values.part3,
+					values.part4,
+				),
+			'색상 이름': values => {
+				if (isMatch(values)) return searchNameList[0]?.hex;
+				else return null;
+			},
 		};
-	
-		const convertToHex = converters[selectedLabel] || ((values) => values);
-	
+
+		const convertToHex = converters[selectedLabel] || (values => values);
+
 		const hexValue = convertToHex(inputValues);
 		setInputColorValue(hexValue);
 	};
+
+	const isMatch = values => {
+		return (
+			searchNameList[0]?.name.toLocaleUpperCase() ===
+				values.part1.toLocaleUpperCase() ||
+			searchNameList[0]?.korean_name === values.part1
+		);
+	};
+
+	// inputColorValue가 변경됐다는 건 검색 버튼이 눌렸다는 것
 	useEffect(() => {
-		onPressSearch();
+		if (inputColorValue) onPressSearch();
+		setInputColorValue('');
 	}, [inputColorValue]);
+
+	// 파일 내 색상 이름 검색
+	const searchInputColorName = keyword => {
+		if (!keyword) {
+			setSearchNameList([]);
+			return;
+		}
+
+		if (isValidKorean(keyword)) {
+			setSearchNameList(getSearchColorList('korean_name', keyword));
+		} else {
+			setSearchNameList(
+				getSearchColorList('name', keyword.toLocaleUpperCase()),
+			);
+		}
+	};
+
+	// 자동완성 검색 리스트 찾기
+	useEffect(() => {
+		if (selectedLabel === '색상 이름') {
+			searchInputColorName(inputValues.part1);
+		}
+	}, [inputValues.part1]);
+
+	// 자동완성 검색 리스트 터치시
+	const handlePressSearchList = label =>{
+		setInputValues({ ...inputValues, part1: label })
+		setSearchNameList([]);
+	};
+
+	// 자동완성 검색 리스트 초기화
+	useEffect(() => {
+		setSearchNameList([]);
+	}, [selectedLabel]);
 
 	return (
 		<View>
@@ -80,19 +143,56 @@ const SearchModal = ({
 								inputValues={inputValues}
 								setInputValues={setInputValues}
 							/>
+
+							{/* 자동완성 추천 검색어 */}
+							{searchNameList.length > 0 &&
+								!(searchNameList.length == 1 && isMatch(inputValues)) && (
+									<ScrollView
+										style={{
+											borderColor: COLOR.GRAY_6,
+											borderRadius: 8,
+											borderWidth: 1,
+										}}
+										showsVerticalScrollIndicator={false}
+										keyboardShouldPersistTaps={'always'}>
+										{searchNameList.map(l => (
+											<ListValue
+												key={l.hex}
+												label={l.korean_name || l.name}
+												onPressLabel={
+													handlePressSearchList
+												}
+											/>
+										))}
+									</ScrollView>
+								)}
 						</View>
 					</View>
 					<View style={styles.buttonContainer}>
 						<TouchableOpacity
-							style={styles.cancelButton}
+							style={[
+								styles.modalButton,
+								{
+									backgroundColor: COLOR.GRAY_6,
+									borderBottomLeftRadius: 8,
+								},
+							]}
 							onPress={handleCloseModal}>
-							<Text style={{ color: COLOR.WHITE }}>이전으로</Text>
+							<ArrowGoBackSVG color={COLOR.WHITE} />
+							<Text style={styles.buttonText}> 이전으로</Text>
 						</TouchableOpacity>
 
 						<TouchableOpacity
-							style={styles.searchButton}
+							style={[
+								styles.modalButton,
+								{
+									backgroundColor: COLOR.PRIMARY,
+									borderBottomRightRadius: 8,
+								},
+							]}
 							onPress={handlePressSearch}>
-							<Text style={{ color: COLOR.WHITE }}>검색하기</Text>
+							<Formkit_submitSVG color={COLOR.WHITE} />
+							<Text style={styles.buttonText}> 검색하기</Text>
 						</TouchableOpacity>
 					</View>
 				</View>
@@ -160,21 +260,16 @@ const styles = StyleSheet.create({
 		marginTop: 18,
 		flexDirection: 'row',
 	},
-	cancelButton: {
+	modalButton: {
 		width: '50%',
 		height: '100%',
+		flexDirection: 'row',
 		alignItems: 'center',
 		justifyContent: 'center',
-		backgroundColor: COLOR.GRAY_6,
-		borderBottomLeftRadius: 8,
 	},
-	searchButton: {
-		width: '50%',
-		height: '100%',
-		alignItems: 'center',
-		justifyContent: 'center',
-		backgroundColor: COLOR.PRIMARY,
-		borderBottomRightRadius: 8,
+	buttonText: {
+		fontFamily: 'Pretendard-Bold',
+		color: COLOR.WHITE,
 	},
 });
 
