8d1008c144486e3aa74b1624c7122dadd20a1e9d
xogus7 <xogus2676@gmail.com>
2024-09-09 03:57:59 +0900

    Design: icons svg 파일 추가

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,8 +3,14 @@ import ImageSVG from '@icons/image.svg'
 import AISVG from '@icons/ai.svg'
 import PaletteSVG from '@icons/palette.svg'
 import ReportSVG from '@icons/report.svg'
+import ArrowGoBackSVG from '@icons/arrow_go_back.svg'
+import FormkitSubmitSVG from '@icons/formkit_submit.svg'
+import SearchSVG from '@icons/search.svg'
+
 import Img from '@icons/img.png'
 import Img2 from '@icons/img2.png'
 import Img3 from '@icons/img3.png'
 
-export { CameraSVG, ImageSVG, AISVG, PaletteSVG, ReportSVG, Img, Img2, Img3 }
+export { CameraSVG, ImageSVG, AISVG, PaletteSVG, ReportSVG,
+    Img, Img2, Img3,
+    ArrowGoBackSVG, FormkitSubmitSVG, SearchSVG }
