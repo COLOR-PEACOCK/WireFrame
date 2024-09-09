c3c228c5bc6bc67a2f687e0565ae944d0d79fce8
xogus7 <xogus2676@gmail.com>
2024-09-09 09:31:30 +0900

    Fix: GestureHandlerRootView 오류로 추가
    아래 라이브러리 사용하기 위함
    - Carousel Pagination.Custom
    - react-native-reanimated

--- App.jsx
+++ App.jsx
@@ -1,13 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import Router from './src/router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 function App() {
-  return (
-    <NavigationContainer>
-      <Router />
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<GestureHandlerRootView>
+				<Router />
+			</GestureHandlerRootView>
+		</NavigationContainer>
+	);
 }
 
 export default App;
