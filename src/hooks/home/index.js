c400e21eefa69ce65df2cc5413bdc3d712ef5c46
xogus7 <xogus2676@gmail.com>
2024-09-11 20:58:01 +0900

    Refactor: useBackHandler re-render

--- src/hooks/home/index.js
+++ src/hooks/home/index.js
@@ -1,4 +1,4 @@
 import useInputState from '@hooks/home/useInputState';
 import usePressButtonState from '@hooks/home/usePressButtonState';
-
-export { useInputState, usePressButtonState }
+import useBackHandler from '@hooks/home/useBackHandler';
+export { useInputState, usePressButtonState, useBackHandler }
