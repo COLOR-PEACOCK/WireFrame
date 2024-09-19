1c836014228bd0e9243494f1064a0d50a8345db6
xogus7 <xogus2676@gmail.com>
2024-09-19 23:08:50 +0900

    Remove: indicator.jsx

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,7 @@
 import PressButton from '@components/Home/PressButton';
 import OutlinedText from '@components/Home/OutlinedText';
-import Indicator from '@components/Home/Indicator';
 import ListValue from '@components/Home/ListValue';
 import Dropdown from '@components/Home/Dropdown';
 import SearchInputForm from '@components/Home/SearchInputForm'
 import SearchModal from '@components/Home/SearchModal';
-export { PressButton, OutlinedText, Indicator, ListValue, Dropdown, SearchInputForm, SearchModal };
+export { PressButton, OutlinedText, ListValue, Dropdown, SearchInputForm, SearchModal };
