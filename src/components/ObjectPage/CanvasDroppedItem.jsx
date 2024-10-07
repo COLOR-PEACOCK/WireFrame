c4a2800809ead912cb841d6df3581ecfe8e7e026
Dos98 <109190087+doseong98@users.noreply.github.com>
2024-10-07 20:18:57 +0900

    Design: 액티브 아이콘 반응형 적용

--- src/components/ObjectPage/CanvasDroppedItem.jsx
+++ src/components/ObjectPage/CanvasDroppedItem.jsx
@@ -28,12 +28,17 @@ const CanvasDroppedItem = ({ item, isSelected, onSelect, onDelete }) => {
 
 	const getFocusButtonPosition = category => {
 		const positions = {
-			clothesTop: { top: -352, right: -142 },
-			clothesBottom: { top: -230, right: -142 },
-			shoes: { top: -90, right: -150 },
-			socks: { top: -100, right: 90 },
+			clothesTop: { top: heightScale(-352), right: heightScale(-148) },
+			clothesBottom: { top: heightScale(-230), right: heightScale(-148) },
+			shoes: { top: heightScale(-90), right: heightScale(-148) },
+			socks: { top: heightScale(-100), right: heightScale(90) },
 		};
-		return positions[category] || { top: -90, right: -100 };
+		return (
+			positions[category] || {
+				top: heightScale(-90),
+				right: heightScale(-100),
+			}
+		);
 	};
 
 	const getCategoryIcon = (category, isVisible) => {
@@ -70,7 +75,7 @@ const CanvasDroppedItem = ({ item, isSelected, onSelect, onDelete }) => {
 				onPress={onSelect}>
 				<Image
 					source={getCategoryIcon(item.category, item.isVisible)}
-					style={{ width: 28, height: 28 }}
+					style={{ width: heightScale(28), height: heightScale(28) }}
 				/>
 			</TouchableOpacity>
 			{isSelected && item.category !== 'socks' && (
@@ -78,14 +83,17 @@ const CanvasDroppedItem = ({ item, isSelected, onSelect, onDelete }) => {
 					style={[
 						styles.trashButton,
 						{
-							top: focusButtonPosition.top - 3,
-							right: focusButtonPosition.right - 49,
+							top: focusButtonPosition.top - heightScale(3),
+							right: focusButtonPosition.right - heightScale(49),
 						},
 					]}
 					onPress={() => onDelete(item.id)}>
 					<Image
 						source={TrashIcon}
-						style={{ width: 15, height: 23 }}
+						style={{
+							width: heightScale(15),
+							height: heightScale(23),
+						}}
 					/>
 				</TouchableOpacity>
 			)}
@@ -100,11 +108,11 @@ const styles = StyleSheet.create({
 	focusButton: {
 		position: 'absolute',
 		backgroundColor: COLOR.GRAY_1,
-		borderRadius: 50,
-		borderWidth: 2,
+		borderRadius: heightScale(50),
+		borderWidth: heightScale(2),
 		borderColor: COLOR.GRAY_4,
-		width: 48,
-		height: 48,
+		width: heightScale(48),
+		height: heightScale(48),
 		justifyContent: 'center',
 		alignItems: 'center',
 		zIndex: 1001,
@@ -117,11 +125,11 @@ const styles = StyleSheet.create({
 		backgroundColor: COLOR.GRAY_3,
 		flexDirection: 'row',
 		borderRadius: 50,
-		width: 100,
-		height: 54,
+		width: heightScale(100),
+		height: heightScale(54),
 		justifyContent: 'flex-end',
 		alignItems: 'center',
-		paddingRight: 20,
+		paddingRight: heightScale(20),
 		zIndex: 1000,
 	},
 });
